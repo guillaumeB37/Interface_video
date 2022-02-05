5785a0b5ea4c3325b50d77761cd5ce6d5038c08b
PearlClone <benjaminlecomte37@gmail.com>
2022-02-05 20:33:59 +0100

    - les reufs j'vous ai fais une dinguerie là juré - dark/light mode finished

--- Interface_video/theme.js
+++ Interface_video/theme.js
@@ -1,3 +1,45 @@
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+console.log(darkMode);
+
+const enableDarkMode = () => {
+  // add the class darkmode to the body
+  document.body.classList.add("darkMode")
+  // update darkMode in the localStorage
+  localStorage.setItem("darkMode", "enable");
+};
+
+const disableDarkMode = () => {
+  // add the class darkmode to the body
+  document.body.classList.remove("darkMode")
+  // update darkMode in the localStorage
+  localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enable") {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode');
+  if (darkMode !== "enable") {
+    enableDarkMode();
+    console.log(darkMode);
+  } else {
+    disableDarkMode();
+    console.log(darkMode);
+  }
+});
+
+
+
+
+
+/*var count = 0;
+sessionStorage.setItem('count', count);
+console.log(sessionStorage.getItem('count'));
+
 document.querySelector('.switch_theme').addEventListener
 ('click', () => {
   var header = document.getElementById("header");
@@ -8,6 +50,16 @@ document.querySelector('.switch_theme').addEventListener
   main.classList.toggle('dark');
   var button = document.getElementById("button-switch-theme");
   button.classList.toggle('bx-rotate-180')
-});
+  count ++;
+});*/
 
-document.querySelector('.')
+/*window.onload = function() {
+  var header = document.getElementById("header");
+  header.classList.toggle('dark');
+  var body = document.getElementById("body");
+  body.classList.toggle('dark');
+  var main = document.getElementById("main");
+  main.classList.toggle('dark');
+  var button = document.getElementById("button-switch-theme");
+  button.classList.toggle('bx-rotate-180')
+};*/
