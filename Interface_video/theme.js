1c0c5db1a1ecb5dafa69be742bf3924c0662160d
PearlClone <benjaminlecomte37@gmail.com>
2022-02-05 18:33:49 +0100

    -finish navbar & switch theme button -clean images folder -rename .js file

---
+++ Interface_video/theme.js
@@ -0,0 +1,11 @@
+document.querySelector('.switch_theme').addEventListener
+('click', () => {
+  var header = document.getElementById("header");
+  header.classList.toggle('dark');
+  var body = document.getElementById("body");
+  body.classList.toggle('dark');
+  var main = document.getElementById("main");
+  main.classList.toggle('dark');
+  var button = document.getElementById("button-switch-theme");
+  button.classList.toggle('bx-rotate-180')
+})
