8e92c4bc4a15b4497992466715d7287dc28c6c99
PearlClone <benjaminlecomte37@gmail.com>
2022-02-08 16:26:29 +0100

    - rangement des fichiers des des dossiers approprie - avancement de la page d'acceuil

--- Interface_video/js/theme.js
+++ Interface_video/js/theme.js
@@ -35,35 +35,3 @@ darkModeToggle.addEventListener('click', () => {
     console.log(darkMode);
   }
 });
-
-
-
-
-
-/*var count = 0;
-sessionStorage.setItem('count', count);
-console.log(sessionStorage.getItem('count'));
-
-document.querySelector('.switch_theme').addEventListener
-('click', () => {
-  var header = document.getElementById("header");
-  header.classList.toggle('dark');
-  var body = document.getElementById("body");
-  body.classList.toggle('dark');
-  var main = document.getElementById("main");
-  main.classList.toggle('dark');
-  var button = document.getElementById("button-switch-theme");
-  button.classList.toggle('bx-rotate-180')
-  count ++;
-});*/
-
-/*window.onload = function() {
-  var header = document.getElementById("header");
-  header.classList.toggle('dark');
-  var body = document.getElementById("body");
-  body.classList.toggle('dark');
-  var main = document.getElementById("main");
-  main.classList.toggle('dark');
-  var button = document.getElementById("button-switch-theme");
-  button.classList.toggle('bx-rotate-180')
-};*/
