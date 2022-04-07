a47d07b4ac839292deecc31e649581ca261eb8a3
Guillaume BRUNET <guillaume.brunet4@lyc-rabelais-chinon.ac-orleans-tours.fr>
2022-04-07 15:48:18 +0200

    fin du formulaire

--- Interface_video/php/index.php
+++ Interface_video/php/index.php
@@ -6,7 +6,7 @@
         <h1>Ecriture fichier csv</h1>
             <?php
                 $f = fopen ("fichiertexte.csv","a+");
-                fputs($f,$_POST['Nom'].';'.$_POST['Prenom'].';'.$_POST['Adresse'].';'.$_POST['Code_Postal'].';'.$_POST['Ville'].';'.$_POST['Telephone'].';'.$_POST['Email'].';'.$_POST['Message'].';')
+                fputs($f,$_POST['Nom'].';'.$_POST['Prenom'].';'.$_POST['Adresse'].';'.$_POST['Code_Postal'].';'.$_POST['Ville'].';'.$_POST['Telephone'].';'.$_POST['E-Mail'].';'.$_POST['Message'].';')
                 fclose($f);
 
 
